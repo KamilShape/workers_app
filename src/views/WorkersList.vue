38dda37c17df2ef455675480f6d2c5ef64a3be25
KamilShape <kamilshape@gmail.com>
2021-11-04 13:47:02 +0100

    Adding styles for AddWorker and WorkersList

---
+++ src/views/WorkersList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="workersList">
+    <h1 class="mb-3">Workers List</h1>
+    <table class="table table-success table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Last Name</th>
+      <th scope="col">Position</th>
+      <th scope="col">Salary</th>
+      <th scope="col">Date of Employee</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </tbody>
+</table>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="css">
+  .workersList{
+    width: 90%;
+    margin: 0 auto;
+  }
+</style>
